b32f543bcccf5fb4e11e59194e10e633d583ecfa
Kaietan <34032203+kaietanpaul@users.noreply.github.com>
2024-03-24 17:16:04 +0000

    printf - ground for printf - basic function to output chars

---
+++ ft_printf.c
@@ -0,0 +1,36 @@
+#include <stdarg.h>
+#include <unistd.h>
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	arg_box;
+	int	printed_chars;
+	int	index;
+	char	c;
+
+	index = 0;
+
+	va_start(arg_box, format);
+	c = va_arg(arg_box, int);
+
+	while (format[index] != '\0')
+	{
+		if (format[index] == '%' && format[index + 1] == 'c')
+		{
+			printed_chars += write(1, &c, 1);
+			index++;
+		}
+		else
+			printed_chars += write(1, &format[index], 1);
+		index++;
+		printed_chars++;
+	}
+	va_end(arg_box);
+	return (printed_chars);
+}
+int main(void)
+{
+	ft_printf("Hello, World!\n");
+	ft_printf("Character: %c %c % c\n", 'U');
+	return (0);
+}
