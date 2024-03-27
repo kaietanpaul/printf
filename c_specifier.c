9c4fbb487b8a747c3b6911c4ba03758442c5c68c
Kaietan <34032203+kaietanpaul@users.noreply.github.com>
2024-03-27 16:25:26 +0000

    %c - update

--- c_specifier.c
+++ c_specifier.c
@@ -1,39 +1,6 @@
-#include <stdarg.h>
 #include <unistd.h>
 
-int	c_specifier(const char *format, ...)
+int c_specifier(int c)
 {
-	va_list	arg_box;
-	int	printed_chars;
-	int	index;
-	char	c;
-
-	index = 0;
-	printed_chars = 0;
-	va_start(arg_box, format);
-
-	while (format[index] != '\0')
-	{
-		if (format[index] == '%' && format[index + 1] == 'c')
-		{
-			c = va_arg(arg_box, int);
-			printed_chars += write(1, &c, 1);
-			index++;
-		}
-		else
-			printed_chars += write(1, &format[index], 1);
-		index++;
-	}
-
-	va_end(arg_box);
-
-	return (printed_chars);
-}
-
-int main(void)
-{
-	c_specifier("Hello, World!\n");
-	c_specifier("Character: %c %c %% cc%%c\n", 'U', 'B', 'A', 'D');
-
-	return (0);
+	return (write(1, &c, 1));
 }
