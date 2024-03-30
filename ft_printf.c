5a5ae1475d4f8145fe31ca85b88de9a4370ac7e1
Kaietan <34032203+kaietanpaul@users.noreply.github.com>
2024-03-30 18:05:31 +0000

    %id - update

--- ft_printf.c
+++ ft_printf.c
@@ -1,8 +1,10 @@
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdio.h>
+
 #include "c_specifier.c"
 #include "s_specifier.c"
-
+#include "id_specifier.c"
 
 static int check_specifier(va_list arg_box, char specifier) {
 	int len;
@@ -12,12 +14,15 @@ static int check_specifier(va_list arg_box, char specifier) {
 		len += c_specifier(va_arg(arg_box, int));
 	else if (specifier == 's')
 		len += s_specifier(va_arg(arg_box, char *));
-	// else if (specifier == 'i' || specifier == 'd')
-	// 	len += id_specifier(va_arg(arg_box, int));
+	else if (specifier == '%')
+		len = write(1, "%", 1);
+	else if (specifier == 'i' || specifier == 'd')
+		len += id_specifier(va_arg(arg_box, int));
 	// else if (specifier == 'x' || specifier == 'X')
 	// 	len += id_specifier(va_arg(arg_box, unsigned int));
 	// else if (specifier == 'p')
 	// 	len += p_specifier(va_arg(arg_box, unsigned int));
+
 	return (len);
 }
 
@@ -35,11 +40,15 @@ int ft_printf(const char *format, ...) {
 	{
 		if (format[index] == '%')
 		{
+			len += check_specifier(arg_box, format[index + 1]);
 			index++;
-			len += check_specifier(arg_box, format[index]);
+
 		}
 		else
+		{
 			len += write(1, &format[index], 1);
+
+		}
 		index++;
 	}
 	va_end(arg_box);
@@ -48,9 +57,9 @@ int ft_printf(const char *format, ...) {
 
 int	main(void)
 {
-	
-	ft_printf("C: 1234%c6\n", 53);
+	ft_printf("C: 1234%c6\n", '5');
 	ft_printf("S: 1234%ssix\n", "five");
-
-	return (0);
+	ft_printf("Percent: 1234%%6\n");
+	ft_printf("id: 1234%i6%d\n", 5, 78);
+	return 0;
 }
---
+++ id_specifier.c
@@ -0,0 +1,47 @@
+#include <unistd.h>
+#include <stdio.h>
+
+static	int edge_case(int c)
+{
+	if (c == 0)
+		return write(1, "0", 1);
+	else if (c == -2147483648)
+		return write(1, "-2147483648", 11);
+	return 0;
+}
+
+int	id_specifier(int c)
+{
+	int	printed_chars;
+	char	new_num;
+
+	printed_chars = 0;
+	printed_chars += edge_case(c);
+	if (printed_chars > 0)
+		return (printed_chars);
+	if (c < 0)
+	{
+		c = -c;
+		printed_chars += write(1, "-", 1);
+		printed_chars += id_specifier(c);
+	}
+	else if (c > 9)
+	{
+		new_num = c % 10 + '0';
+		printed_chars += id_specifier(c /= 10);
+		printed_chars += write(1, &new_num, 1);
+	}
+	else
+	{
+		new_num = c % 10 + '0';
+		printed_chars += write(1, &new_num, 1);
+	}
+	return (printed_chars);
+}
+int main(void)
+{
+	int printed_chars;
+	printed_chars = id_specifier(-01 + 200);
+	printf("\n%d\n", printed_chars);
+	return (0);
+}
