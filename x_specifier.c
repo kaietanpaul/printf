9e4baab1277e8ea971344b054459a80b174fa497
Kaietan <34032203+kaietanpaul@users.noreply.github.com>
2024-03-31 16:47:28 +0000

    %xX - ready and working + norminette update

--- x_specifier.c
+++ x_specifier.c
@@ -1,42 +1,43 @@
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdio.h>
 
-static void print_hex_recursive(unsigned int n, int *printed_chars) {
-	char *hex_digits = "0123456789abcdef";
+static int	print_hex(unsigned int c, char hex_chars[])
+{
+	int		printed_chars;
+	int		reminder;
+	char	hex_digit;
 
-	if (n >= 16) {
-		print_hex_recursive(n / 16, printed_chars);
+	reminder = 0;
+	printed_chars = 0;
+	if (c > 0)
+	{
+		reminder = c % 16;
+		printed_chars += print_hex(c / 16, hex_chars);
 	}
-	*printed_chars += write(1, &hex_digits[n % 16], 1);
+	if (reminder != 0)
+	{
+		hex_digit = hex_chars[reminder];
+		printed_chars += write(1, &hex_digit, 1);
+	}
+	return (printed_chars);
 }
 
-int x_specifier(const char *format, ...) {
-	va_list arg_box;
-	int printed_chars = 0;
-	int index = 0;
-	unsigned int u;
+int	x_specifier(unsigned int c, char specifier)
+{
+	int			printed_chars;
+	static char	small_hex[] = "0123456789abcdef";
+	static char	large_hex[] = "0123456789ABCDEF";
 
-	va_start(arg_box, format);
-
-	while (format[index] != '\0') {
-		if (format[index] == '%' && format[index + 1] == 'x') {
-			u = va_arg(arg_box, unsigned int);
-			print_hex_recursive(u, &printed_chars);
-			index++;
-		} else {
-			printed_chars += write(1, &format[index], 1);
-		}
-		index++;
+	printed_chars = 0;
+	if (c == 0)
+	{
+		printed_chars += write(1, "0", 1);
+		return (printed_chars);
 	}
-
-	va_end(arg_box);
-
-	return printed_chars;
-}
-
-int main(void) {
-	x_specifier("Hello, World!\n");
-	x_specifier("Hexadecimal: %x\n", 9);
-
-	return 0;
+	if (specifier == 'x')
+		printed_chars += print_hex(c, small_hex);
+	else
+		printed_chars += print_hex(c, large_hex);
+	return (printed_chars);
 }
