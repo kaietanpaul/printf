534661b41ad24217a35a3836e2392f8f164577be
Kaietan <34032203+kaietanpaul@users.noreply.github.com>
2024-03-31 16:48:47 +0000

    %id - norminette update

--- id_specifier.c
+++ id_specifier.c
@@ -1,50 +1,40 @@
 #include <unistd.h>
 #include <stdio.h>
 
-static	int edge_case(int c)
+static int	edge_case(int c)
 {
 	if (c == 0)
-		return write(1, "0", 1);
+		return (write(1, "0", 1));
 	else if (c == -2147483648)
-		return write(1, "-2147483648", 11);
-	return 0;
+		return (write(1, "-2147483648", 11));
+	return (0);
+}
+
+static int	print_digits(int c)
+{
+	int		printed_chars;
+	char	new_num;
+
+	printed_chars = 0;
+	if (c > 9)
+		printed_chars += print_digits(c / 10);
+	new_num = c % 10 + '0';
+	printed_chars += write(1, &new_num, 1);
+	return (printed_chars);
 }
 
 int	id_specifier(int c)
 {
 	int	printed_chars;
-	char	new_num;
 
-	printed_chars = 0;
-	printed_chars += edge_case(c);
+	printed_chars = edge_case(c);
 	if (printed_chars > 0)
 		return (printed_chars);
 	if (c < 0)
 	{
 		c = -c;
 		printed_chars += write(1, "-", 1);
-		printed_chars += id_specifier(c);
 	}
-	else if (c > 9)
-	{
-		new_num = c % 10 + '0';
-		c /= 10;
-		printed_chars += id_specifier(c);
-		printed_chars += write(1, &new_num, 1);
-	}
-	else
-	{
-		new_num = c % 10 + '0';
-		printed_chars += write(1, &new_num, 1);
-	}
+	printed_chars += print_digits(c);
 	return (printed_chars);
-}
-
-int	main(void)
-{
-	int	printed_chars;
-
-	printed_chars = id_specifier(-4535);
-	printf("\n%d\n", printed_chars);
-	return (0);
 }
