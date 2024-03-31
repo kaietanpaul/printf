94fa0f65278b1edfcecb11ea0c74fdba346993b6
Kaietan <34032203+kaietanpaul@users.noreply.github.com>
2024-03-31 16:49:16 +0000

    main - updated for %xX + norminette

--- ft_printf.c
+++ ft_printf.c
@@ -5,9 +5,11 @@
 #include "c_specifier.c"
 #include "s_specifier.c"
 #include "id_specifier.c"
+#include "x_specifier.c"
 
-static int check_specifier(va_list arg_box, char specifier) {
-	int len;
+static int	check_specifier(va_list arg_box, char specifier)
+{
+	int	len;
 
 	len = 0;
 	if (specifier == 'c')
@@ -18,22 +20,20 @@ static int check_specifier(va_list arg_box, char specifier) {
 		len = write(1, "%", 1);
 	else if (specifier == 'i' || specifier == 'd')
 		len += id_specifier(va_arg(arg_box, int));
-	// else if (specifier == 'x' || specifier == 'X')
-	// 	len += id_specifier(va_arg(arg_box, unsigned int));
+	else if (specifier == 'x' || specifier == 'X')
+		len += x_specifier(va_arg(arg_box, unsigned int), specifier);
 	// else if (specifier == 'p')
 	// 	len += p_specifier(va_arg(arg_box, unsigned int));
-
 	return (len);
 }
 
-int ft_printf(const char *format, ...) {
-	va_list arg_box;
-	int index;
-	int len;
-
+int	ft_printf(const char *format, ...)
+{
+	va_list	arg_box;
+	int		index;
+	int		len;
 
 	va_start(arg_box, format);
-
 	index = 0;
 	len = 0;
 	while (format[index] != '\0')
@@ -42,13 +42,9 @@ int ft_printf(const char *format, ...) {
 		{
 			len += check_specifier(arg_box, format[index + 1]);
 			index++;
-
 		}
 		else
-		{
 			len += write(1, &format[index], 1);
-
-		}
 		index++;
 	}
 	va_end(arg_box);
@@ -60,6 +56,7 @@ int	main(void)
 	ft_printf("C: 1234%c6\n", '5');
 	ft_printf("S: 1234%ssix\n", "five");
 	ft_printf("Percent: 1234%%6\n");
-	ft_printf("id: 1234%i6%d\n", 5, 78);
+	ft_printf("id: 1234%i6%d%d\n", 5, 78, -2147483648 );
+	ft_printf("xX: 1234%x and 9,10,%X\n", 5, 17);
 	return 0;
 }
