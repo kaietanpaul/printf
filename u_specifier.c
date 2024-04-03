6450844c43edc2b620c2589b6cdbe64f44d723ee
Kaietan <34032203+kaietanpaul@users.noreply.github.com>
2024-04-03 21:32:23 +0000

    %u - unsigned blueprint

---
+++ u_specifier.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int	u_specifier(unsigned int c)
+{
+	unsigned int	printed_chars;
+	unsigned char new_num;
+
+	printed_chars = 0;
+	if (c == 0)
+		return (printed_chars += write(1, "0", 1));
+	else
+	{
+		printed_chars += u_specifier(c / 10);
+		new_num = c % 10 + '0';
+		printed_chars += write(1, &new_num, 1);
+	}
+	return (printed_chars);
+}
+
+int main(void)
+{
+	u_specifier(255);
+	return (0);
+}
