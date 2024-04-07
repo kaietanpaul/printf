3f0a2d609d0f2e7b96f24d8ac0ad27a80eea19b7
Kaietan <34032203+kaietanpaul@users.noreply.github.com>
2024-04-07 14:00:30 +0000

    %x - update from uns int to uns ln ln for sake of %p

--- x_specifier.c
+++ x_specifier.c
@@ -1,29 +1,19 @@
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdio.h>
+#include "ft_printf.h"
 
-static int	print_hex(unsigned int c, char hex_chars[])
+static int	print_hex(unsigned long long c, char hex_chars[])
 {
 	int		printed_chars;
-	int		reminder;
 	char	hex_digit;
 
-	reminder = 0;
 	printed_chars = 0;
-	if (c > 0)
-	{
-		reminder = c % 16;
+	if (c / 16 > 0)
 		printed_chars += print_hex(c / 16, hex_chars);
-	}
-	if (reminder != 0)
-	{
-		hex_digit = hex_chars[reminder];
-		printed_chars += write(1, &hex_digit, 1);
-	}
+	hex_digit = hex_chars[c % 16];
+	printed_chars += write(1, &hex_digit, 1);
 	return (printed_chars);
 }
 
-int	x_specifier(unsigned int c, char specifier)
+int	x_specifier(unsigned long long c, char specifier)
 {
 	int			printed_chars;
 	static char	small_hex[] = "0123456789abcdef";
