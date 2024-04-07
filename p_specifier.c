d9d30d4b61dd673906b7869ecd78de818122dcbd
Kaietan <34032203+kaietanpaul@users.noreply.github.com>
2024-04-07 13:59:40 +0000

    %p - done & working

---
+++ p_specifier.c
@@ -0,0 +1,16 @@
+#include "ft_printf.h"
+
+int	p_specifier(void *address)
+{
+	int	printed_chars;
+
+	printed_chars = 0;
+	if (address == 0)
+		return (printed_chars += write(1, "0x0", 3));
+	else
+	{
+		printed_chars += write(1, "0x", 2);
+		printed_chars += x_specifier((unsigned long long)address, 'x');
+	}
+	return (printed_chars);
+}
